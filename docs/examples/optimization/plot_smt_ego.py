7213d53b47584ae4f3304e1be23d7702b6cadcb6
Matthias De Lozzo <matthias.delozzo@irt-saintexupery.com>
2024-10-09 16:44:24 +0000

    EGO algorithm from SMT

---
+++ docs/examples/optimization/plot_smt_ego.py
@@ -0,0 +1,98 @@
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This work is licensed under a BSD 0-Clause License.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+# FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""# Surrogate-based optimization using SMT.
+
+The [surrogate modeling toolbox (SMT)](../../../user_guide/regression/smt.md)
+is an open-source Python package for surrogate modeling,
+with a focus on derivatives.
+Bayesian optimization features are also available through its `EGO` class,
+with various acquisition criteria and strategies to acquire points in parallel.
+"""
+
+from __future__ import annotations
+
+from gemseo import configure_logger
+from gemseo import execute_algo
+from gemseo.post.dataset.zvsxy import ZvsXY
+from gemseo.problems.dataset.rosenbrock import create_rosenbrock_dataset
+from gemseo.problems.optimization.rosenbrock import Rosenbrock
+
+configure_logger()
+
+# %%
+# In this example,
+# we seek to minimize the Rosenbrock function $f(x,y)=(1-x)^2+100(y-x^2)^2$
+# over the design space $[-2,2]$.
+# First,
+# we instantiate the problem with $(0, 0)$ as initial guess:
+problem = Rosenbrock()
+
+# %%
+# Then,
+# we minimize the Rosenbrock function using
+#
+# - the `"SMT_EGO"` algorithm,
+# - a maximum number of iterations equal to 40,
+#   including the initial evaluation at the center of the design space
+#   (this first point is common to all optimization algorithms)
+#   and the initial training dataset,
+# - its default settings,
+#   namely
+#
+#   - the expected improvement as acquisition criterion,
+#   - 1 point acquired at a time,
+#   - the Kriging-based surrogate model `"KRG"`,
+#   - 10 initial training points based on a latin hypercube sampling (LHS) technique,
+#   - a multi-start local optimization of the acquisition criterion
+#     from 50 start points with a limit of 20 iterations per local optimization.
+execute_algo(problem, "SMT_EGO", max_iter=40)
+
+# %%
+# We can see in the log
+# that the solution is close to the theoretical one $(x^*,f^*)=((1,1),0)$.
+#
+# We can also visualize all the evaluations
+# and note that most of the points have been added in the valley as expected:
+optimization_history = problem.to_dataset()
+
+initial_point = optimization_history[0:1]
+initial_point.name = "Initial point"
+
+initial_training_points = optimization_history[1:12]
+initial_training_points.name = "Initial training points"
+
+acquired_points = optimization_history[12:]
+acquired_points.name = "Acquired points"
+
+visualization = ZvsXY(
+    create_rosenbrock_dataset(900),
+    ("x", 0),
+    ("x", 1),
+    "rosen",
+    fill=False,
+    other_datasets=(initial_point, initial_training_points, acquired_points),
+)
+visualization.execute(save=False, show=True)
+
+# %%
+# Lastly,
+# we can compare the solution to the one obtained with COBYLA,
+# which is another popular gradient-free optimization algorithm:
+execute_algo(Rosenbrock(), "NLOPT_COBYLA", max_iter=40)
+
+# %%
+# and conclude that for this problem and this initial guess,
+# the surrogate-based algorithm is better than COBYLA.
---
+++ src/gemseo_mlearning/algos/opt/smt/_parallel_evaluator.py
@@ -0,0 +1,69 @@
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Contributors:
+#    AUTHORS:
+#       - Francois Gallard
+"""Parallel evaluation on multiple processes or threads."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Callable
+
+from gemseo.core.parallel_execution.callable_parallel_execution import (
+    CallableParallelExecution,
+)
+from numpy import array
+from smt.applications.ego import Evaluator
+
+if TYPE_CHECKING:
+    from gemseo.typing import RealArray
+
+
+class ParallelEvaluator(Evaluator):
+    """Parallel evaluation on multiple processes or threads."""
+
+    __n_processes: int
+    """The number of processes or threads to use."""
+
+    def __init__(self, n_processes: int) -> None:
+        """
+        Args:
+            n_processes: The number of processes or threads to use.
+        """  # noqa: D205 D212
+        super().__init__()
+        self.__n_processes = n_processes
+
+    def run(
+        self,
+        function: Callable[[RealArray], float],
+        input_values: RealArray,
+    ) -> RealArray:
+        """Evaluate a function on a sample of input points.
+
+        Args:
+            function: The function to evaluate.
+            input_values: The sample of input points.
+
+        Returns:
+            The function evaluations on the sample of points.
+        """
+        if (n_processes := self.__n_processes) > 1:
+            executor = CallableParallelExecution((function,), n_processes=n_processes)
+            output_values = executor.execute(input_values)
+        else:
+            output_values = [function(input_value) for input_value in input_values]
+
+        return array([output_values], dtype="float64").T
